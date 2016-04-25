7799d6ff126c9c83d9d00965262b384ee2b7a94e
pakhandi <asimkprasad@gmail.com>
2016-04-25 13:39:46 +0530

    View all students

--- viewStudents.php
+++ viewStudents.php
@@ -118,61 +118,37 @@
                     <div class="col-md-12">
                         <div class="card">
                             <div class="header">
-                                <h4 class="title">Striped Table</h4>
-                                <p class="category">Here is a subtitle for this table</p>
+                                <h4 class="title">Students</h4>
+                                <p class="category">List of all the students</p>
                             </div>
                             <div class="content table-responsive table-full-width">
                                 <table class="table table-striped">
                                     <thead>
-                                        <th>ID</th>
+                                        <th>Registration Number</th>
                                     	<th>Name</th>
-                                    	<th>Salary</th>
-                                    	<th>Country</th>
-                                    	<th>City</th>
+                                    	<th>Program</th>
+                                    	<th>Contact Number</th>
+                                    	<th>Email Id</th>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                        	<td>1</td>
-                                        	<td>Dakota Rice</td>
-                                        	<td>$36,738</td>
-                                        	<td>Niger</td>
-                                        	<td>Oud-Turnhout</td>
-                                        </tr>
-                                        <tr>
-                                        	<td>2</td>
-                                        	<td>Minerva Hooper</td>
-                                        	<td>$23,789</td>
-                                        	<td>Curaçao</td>
-                                        	<td>Sinaai-Waas</td>
-                                        </tr>
-                                        <tr>
-                                        	<td>3</td>
-                                        	<td>Sage Rodriguez</td>
-                                        	<td>$56,142</td>
-                                        	<td>Netherlands</td>
-                                        	<td>Baileux</td>
-                                        </tr>
-                                        <tr>
-                                        	<td>4</td>
-                                        	<td>Philip Chaney</td>
-                                        	<td>$38,735</td>
-                                        	<td>Korea, South</td>
-                                        	<td>Overland Park</td>
-                                        </tr>
-                                        <tr>
-                                        	<td>5</td>
-                                        	<td>Doris Greene</td>
-                                        	<td>$63,542</td>
-                                        	<td>Malawi</td>
-                                        	<td>Feldkirchen in Kärnten</td>
-                                        </tr>
-                                        <tr>
-                                        	<td>6</td>
-                                        	<td>Mason Porter</td>
-                                        	<td>$78,615</td>
-                                        	<td>Chile</td>
-                                        	<td>Gloucester</td>
-                                        </tr>
+                                        <?php
+                                            $query = "SELECT * FROM studentmaster";
+                                            $allStudents = mysqli_query($connection, $query);
+
+                                            while( $thisStudent = mysqli_fetch_array($allStudents) )
+                                            {
+                                        ?>
+                                                <tr>
+                                                    <td><?php echo $thisStudent['reg_no'] ?></td>
+                                                    <td><?php echo $thisStudent['name'] ?></td>
+                                                    <td><?php echo $thisStudent['program'] ?></td>
+                                                    <td><?php echo $thisStudent['contact_no'] ?></td>
+                                                    <td><?php echo $thisStudent['mail_id'] ?></td>
+                                                </tr>
+                                        <?php
+                                            }
+                                        ?>
+
                                     </tbody>
                                 </table>
 
@@ -181,7 +157,7 @@
                     </div>
 
 
-                    <div class="col-md-12">
+                    <!-- <div class="col-md-12">
                         <div class="card card-plain">
                             <div class="header">
                                 <h4 class="title">Table on Plain Background</h4>
@@ -244,7 +220,7 @@
 
                             </div>
                         </div>
-                    </div>
+                    </div> -->
 
 
                 </div>
