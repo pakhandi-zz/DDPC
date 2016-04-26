239653f39272b1d3ac519c2ef73723e54e9b8bf7
pakhandi <asimkprasad@gmail.com>
2016-04-27 01:54:42 +0530

    Fix Upload doc

--- uploadDoc.php
+++ uploadDoc.php
@@ -28,9 +28,17 @@ if (is_uploaded_file($_FILES['doc']['tmp_name']))
 		$docType = $_POST['application_type'];
 		$currDate = (string)date("Y-m-d");
 		$result = move_uploaded_file($_FILES['doc']['tmp_name'], filesplace."/$name.pdf");
+
+		$query = "SELECT sem_no FROM studentregistration WHERE `reg_no` = '$reg_no'";
+		$record = mysqli_query($connection, $query);
+		$row = mysqli_fetch_array($record);
+		$sem_no = $row['sem_no'];
+
+		$year = "2016";
+
 		if ($result == 1)
 		{
-			$query = "INSERT INTO document (`doc_id`, `application_type`, `date_of_upload`) VALUES ('$name', '$docType', '$currDate')";header('Location: user.php');
+			$query = "INSERT INTO document (`doc_id`, `member_id`, `sem_no`, `academic_year`, `application_type`, `date_of_upload`) VALUES ('$name', '$reg_no', '$sem_no', '$year', '$docType', '$currDate')";
 			//echo $query;
 			mysqli_query($connection, $query);
 			header('Location: user.php');
