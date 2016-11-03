f21ef047758b09623567caf7ad5c3eb86fdf5b79
pakhandi <asimkprasad@gmail.com>
2016-11-03 17:16:19 +0530

    Add roles

--- login.php
+++ login.php
@@ -7,10 +7,11 @@
 		$password = mysqli_real_escape_string($connection, $_POST['password']);
 		$query = "SELECT * FROM studentmaster where reg_no='$reg_no' and password='$password'";
 		$results = mysqli_query($connection, $query);
-		if(mysqli_num_rows($results) > 0)
+		if(mysqli_num_rows($results) == 1)
 		{
 			session_start();
 			$_SESSION['reg_no'] = $reg_no;
+			$_SESSION['role'] = "student";
 			if($reg_no == "20134171")
 			{
 				$_SESSION['is_admin'] = 1;
@@ -19,7 +20,29 @@
 		}
 		else
 		{
-			header("location: ./index.php?invalid=1");
+			$query = "SELECT * FROM faculty where faculty_id='$reg_no' and password='$password'";
+			$results = mysqli_query($connection, $query);
+			if(mysqli_num_rows($results) == 1)
+			{
+				$query = "SELECT * FROM members WHERE member_id='$reg_no'";
+				$results = mysqli_query($connection, $query);
+				if(mysqli_num_rows($results) == 1)
+				{
+					session_start();
+					$_SESSION['reg_no'] = $reg_no;
+					$result = mysqli_fetch_array($results);
+					$_SESSION['role'] = $result['member_type'];
+					header("location: ./dashboard.php");
+				}	
+				else
+				{
+					header("location: ./index.php?invalid=1");
+				}
+			}
+			else
+			{
+				header("location: ./index.php?invalid=1");
+			}
 		}
 	}
 	else
