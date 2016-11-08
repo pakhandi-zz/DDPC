67493649a455bbb061da73528e5d7971709bf04f
pakhandi <asimkprasad@gmail.com>
2016-11-08 19:06:53 +0530

    add target_member

--- addNotification.php
+++ addNotification.php
@@ -1,6 +1,6 @@
 <?php
 
-	session_start();
+    session_start();
     if(!isset($_SESSION['reg_no']))
     {
         header("location: ./");
@@ -13,14 +13,19 @@
         echo "add issue date";
     else
     {
-
+        $target_group = (string) NULL;
+        $target_member = (string) NULL;
         $id = $_GET['id'];
         $description = $_GET['description'];
         $issue_date = $_GET['issue_date'];
+        if(isset($_GET['target_group']))
+            $target_group = $_GET['target_group'];
+        if(isset($_GET['target_member']))
+            $target_member = $_GET['target_member'];
 
         include("./includes/connect.php");
 
-        $query = "INSERT INTO notifications (`id`, `description`, `issue_date`) VALUES('$id', '$description', '$issue_date')";
+        $query = "INSERT INTO notifications (`id`, `description`, `issue_date`, `target_group`, `target_member`) VALUES('$id', '$description', '$issue_date', '$target_group', '$target_member')";
         echo $query;
         $result=mysqli_query($connection, $query); 
         if($result)
