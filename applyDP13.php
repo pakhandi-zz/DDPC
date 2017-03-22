2364b3e6f54d1eaa158b903f8e87c0b23f31a4b0
pakhandi <asimkprasad@gmail.com>
2017-03-22 08:29:14 +0530

    Change fetch_student API and add DP-13
    Note that DP-13 is not complete in any sense

--- applyDP13.php
+++ applyDP13.php
@@ -1,42 +1,28 @@
 	<?php
 
-		include("./includes/preProcess.php");
-		$student_reg_no = $_GET['student_reg_no'];
-		$query = "SELECT * FROM studentmaster NATURAL JOIN currentsupervisor WHERE reg_no='$student_reg_no'";
-		$results = mysqli_query($connection, $query);
-		$student = mysqli_fetch_array($results);
-		$query = "SELECT date_of_reg FROM studentregistration WHERE reg_no ='$reg_no' ORDER BY sem_no ASC";
-		$results = mysqli_query($connection, $query);
-		$arr = mysqli_fetch_array($results);
-		$date_of_reg = $arr['date_of_reg'];
-		if($date_of_reg === null) {
-			$date_of_reg = date('Y-m-d');
-		}
-		$query = "SELECT sem_no FROM studentregistration WHERE reg_no ='$reg_no' ORDER BY sem_no DESC";
-		$results = mysqli_query($connection, $query);
-		if(mysqli_num_rows($results) == 0)
-		{
-		 $current_sem_no = 0;
-		}
-		else
-		{
-		    $arr = mysqli_fetch_array($results);
-		    $current_sem_no = $arr['sem_no'];
-		}
-		$sem_no = $current_sem_no + 1;
-
-		$thisQuery = "SELECT member_id FROM `members` WHERE role='ConvenerDDPC'";
+	include("./includes/preProcess.php");
+	$query = "SELECT * from awarddistribution";
+	$result = mysqli_query($connection, $query);
+	$course_distribution = array();
+	while ($thisSem = mysqli_fetch_assoc($result))
+	{
+		$course_distribution[$thisSem['sem_no']] = explode('/', $thisSem['credits_through']);
+	}
+	$query = "SELECT * from currentsupervisor NATURAL JOIN faculty WHERE reg_no = '$reg_no'";
+	$result = mysqli_query($connection, $query);
+	$supervisor = mysqli_fetch_assoc($result);
+	$supervisor_name = $supervisor['name'];
+	$sem_no = $current_sem_no + 1;
+	if($date_of_reg === null) {
+		$date_of_reg = date('Y-m-d');
+	}
+	if ( !strcmp($_SESSION['role'], "student") )
+	{
+		$thisUniqueId = $_SESSION['reg_no'];
+		$thisQuery = "SELECT supervisor1_id FROM currentsupervisor WHERE reg_no='$thisUniqueId'";
 		$thisResult = mysqli_query($connection, $thisQuery);
 		$thisResult = mysqli_fetch_array($thisResult);
-		$nextNotifTo = $thisResult['member_id'];
-
-		function getFacultyName($faculty_id){
-		include("./includes/connect.php");
-		$query = "SELECT name FROM faculty WHERE faculty_id ='$faculty_id'";
-		$result = mysqli_query($connection, $query);
-		$faculty = mysqli_fetch_assoc($result);
-		$faculty_name = $faculty['name'];
-		return $faculty_name;
+		$nextNotifTo = $thisResult['supervisor1_id'];
 	}
 	?> 
 	<!doctype html>
@@ -71,29 +57,90 @@
 
 		<link href="assets/css/datepicker.css" rel="stylesheet" />
 		<script type="text/javascript">
-			function nowsearch(faculty_id, num)
+			function nowsearch(course_id, num)
 			{
-				var url='./fetch_faculty.php?faculty_id=' + faculty_id;
+				var url='./fetch_course.php?course_id=' + course_id;
 				load_my_URL(url,function(data){
-				var xml=parse_my_XMLdata(data);
-				var Faculty = xml.documentElement.getElementsByTagName("faculty");
-				var name = Faculty[0].getAttribute("name");
-				var dept_name = Faculty[0].getAttribute("dept_name");
-				var designation = Faculty[0].getAttribute("designation");
-				var dept_id = Faculty[0].getAttribute("dept_id");
-				var student_dept_id = <?php echo $student['dept_id'] ?>;
-				var id0 = "0" + num;
-				var id1 = "1" + num;
-				var id2 = "2" + num;
-				document.getElementById(id0).innerHTML = designation;
-				document.getElementById(id1).innerHTML = dept_name;
-				if(dept_id == student_dept_id) {
-					document.getElementById(id2).value = "Internal";
-				} else {
-					document.getElementById(id2).value = "External";
-				}
+					var xml=parse_my_XMLdata(data);
+					var mCourses = xml.documentElement.getElementsByTagName("course");
+					if (mCourses[0].hasAttribute("total_credits"))
+					{
+						var course_name = mCourses[0].getAttribute("course_name");
+						var course_coordinator = mCourses[0].getAttribute("course_coordinator");
+						var total_credits = mCourses[0].getAttribute("total_credits");
+						var id1 = num + "1";
+						var id2 = num + "2";
+						var id3 = num + "3";
+						document.getElementById(id1).setAttribute("min", total_credits);
+						document.getElementById(id1).setAttribute("max", total_credits);
+						document.getElementById(id1).value = total_credits;
+						document.getElementById(id2).innerHTML = mCourses[0].getAttribute("dept_name");
+						document.getElementById(id3).innerHTML = course_coordinator;
+						var s_id = "student_selected_coordinator" + num;
+						document.getElementById(s_id).style.visibility = "hidden";
+
+					} else {
+
+						var course_name = mCourses[0].getAttribute('course_name');
+						var course_coordinator = mCourses[0].getAttribute('course_coordinator');
+						var min_credits = mCourses[0].getAttribute("min_credits");
+						var max_credits = mCourses[0].getAttribute("max_credits");
+						var id1 = num + "1";
+						var id2 = num + "2";
+						var id3 = num + "3";
+						document.getElementById(id1).setAttribute("min", min_credits);
+						document.getElementById(id1).setAttribute("max", max_credits);
+						document.getElementById(id1).value = min_credits;
+						document.getElementById(id2).innerHTML = mCourses[0].getAttribute("dept_name");
+						course_name = (course_name.toLowerCase());
+						if (course_name == "state of the art" || course_name == "soa") {
+							document.getElementById(id3).innerHTML = "Entire SRC Panel";
+							var s_id = "student_selected_coordinator" + num;
+							document.getElementById(s_id).style.visibility = "hidden";
+						} else if (course_name == "comprehensive") {
+							document.getElementById(id3).innerHTML = "Comprehensive Panel";
+							var s_id = "student_selected_coordinator" + num;
+							document.getElementById(s_id).style.visibility = "hidden";
+						} else if (course_name == "thesis performance") {
+							document.getElementById(id3).innerHTML = "<?php echo $supervisor_name; ?>";
+							var s_id = "student_selected_coordinator" + num;
+							document.getElementById(s_id).style.visibility = "hidden";
+						} else if (course_name == "mini project" || course_name == "research seminar") {
+
+							var input_faculty_name = document.getElementById(id3);
+							input_faculty_name.innerHTML = "<?php echo $supervisor_name; ?>";
+							var s_id = "student_selected_coordinator" + num;
+							document.getElementById(s_id).style.visibility = "visible";
+						} 
+					}
+
 				});
 			}
+
+			function instaSearch(student_id, num)
+			{
+				var url='./fetch_student.php?reg_no=' + student_id;
+				load_my_URL(url,function(data){
+				var xml=parse_my_XMLdata(data);
+				var Student = xml.documentElement.getElementsByTagName("student");
+				var studentName = Student[0].getAttribute("name");
+				var studentRegno = Student[0].getAttribute("reg_no");
+				var studentDateofReg = Student[0].getAttribute("date_of_reg");
+				var studentSupervisor2 = Student[0].getAttribute("supervisor2_id");
+
+				var id = num + "1";
+				document.getElementById(id).value = studentRegno;
+				id = num + "2";
+				document.getElementById(id).innerHTML = studentDateofReg;
+				id = num + "3";
+				document.getElementById(id).innerHTML = Student[0].getAttribute("dept_name");
+				id = num + "4";
+				document.getElementById(id).innerHTML = studentSupervisor2;
+				id = num + "5";
+				document.getElementById(id).innerHTML = studentSupervisor2;				
+				});
+			}
+
 			function load_my_URL(url, do_func)
 			{
 				var my_req = window.ActiveXObject ? new ActiveXObject('Microsoft.XMLHTTP') : new XMLHttpRequest;
@@ -162,7 +209,7 @@
 						<li>
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 								<i class="ti-panel"></i>
-									<p style="display : none;">Stats</p>
+								<p style="display : none;">Stats</p>
 							</a>
 						</li>
 						<?php include('./includes/notifications.php'); ?>
@@ -183,102 +230,126 @@
 					<div class="col-md-12">
 						<div class="card">
 							<b>
-								<div class="col-md-offset-10"> Form: DP-08</div>
-								<div class="col-md-offset-10"> (Clause 9, 12.3)</div>
+								<div class="col-md-offset-10"> Form: DP-13</div>
+								<div class="col-md-offset-10"> (Clause 12.1(3))</div>
 								<center><h5><b>Motilal Nehru National Institute of Technology Allahabad</b></h5></center>
-								<center><u><h5>List of Suggested Examiners for Ph.D Comprehensive Examination</h5></u></center><br>
+								<center><u><h5>Supervisor Selection<br />(To be filled by the supervisor)</h5></u></center><br>
 								<div class="col-md-offset-1" style="font-size:15px">
-									<form class="form-inline" id="dp02" name="dp02" action="submitDP08.php" method="post">
+									<form class="form-inline" id="dp01" name="dp01" action="submitDP01.php" method="post" onsubmit="return checkform(this.form);">
 
 
 									</b>
-									Name of the Student : <b><?php echo $student['name']; ?></b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Reg. No. <b><?php echo $student['reg_no'];?> </b><br>
-									Department : <b> Computer Science and Engineering </b><br>Date of First Registration: <b><?php echo $date_of_reg; ?></b><br>
-									Name of Supervisor(s) : <b><?php echo getFacultyName($student['supervisor1_id']); 
-									if(!empty($student['supervisor2_id'])){
-										echo getFacultyName($student['supervisor2_id']); 
-									}
-									?></b>
+
+									<?php
+										// Get the details of the supervisor for the form
+
+										$tempVar = $user['faculty_id'];
+										$tempQuery = "SELECT * FROM faculty WHERE faculty_id='$tempVar'";
+										$tempResult = mysqli_query($connection, $tempQuery);
+										$tempResult = mysqli_fetch_array($tempResult);
+									?>
+
+
+									Name of the Faculty : <b><?php echo $user['name']; ?></b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Designation : <b><?php echo $tempResult['designation'] ?> </b><br>
+									Department : <b> Computer Science and Engineering </b><br>
+									Co-Supervisor (if any): <b><?php echo "Nothing" ?></b><br>
+									<center><u><h5>Details of the Ph.D Students being supervised at present</h5></u></center><br>
 								</div>
 								
 								<div class="row col-md-offset-1">
 									<div class="col-md-11" style="font-size:10px;">
-										<table class="table table-bordered table-condensed" style="font-size:15px">
+										<table class="table table-bordered table-condensed">
 											<thead>
 												<th>SI. No.</th>
-												<th>Name of Examiners</th>
-												<th>Designation</th>
-												<th>Department</th>
+												<th>Name of the Student</th>
+												<th>Reg. No.</th>
+												<th>Date of Registration</th>
+												<th>Department in which registered</th>
+												<th>Co-Supervisor (if any)</th>
+												<th>Status of Research Work</th>
 											</thead>
 											<tbody>
-												<?php
-													$query = "SELECT * FROM faculty NATURAL JOIN department";
-													$faculty = mysqli_query($connection, $query);
-													$thisFaculty = array();
-													while ( $row = mysqli_fetch_array($faculty) ){
-													    $thisFaculty[] = $row;
-													}
-													for( $i = 0; $i < 5; $i++){
-														$j = $i + 1;
-														?>
-														<tr>
-															<td><?php echo $j ?></td>
-															<td><select class="form-control border-input" name="faculty<?php echo $j ?>" 
-														onchange="nowsearch(this.value, <?php echo $j ?>);" required>
-														<option value="">Select</option>
-														<?php
-
-														foreach ($thisFaculty as $key=>$obj) {
-														
-														?>
-															<option value="<?php echo $obj['faculty_id'] ?>"><?php echo $obj['name'] ?></option>
-															<?php
-														}
-														?>
-																</select>	
-															</td>
-														<td><p id=0<?php echo $j ?> ></p></td>
-														<td><p id=1<?php echo $j ?> ></p></td>
-														<input id=2<?php echo $j ?> type="hidden" name="role<?php echo $j ?>" value="" />
-													</tr>
-												<?php
-													}
+												<?php 
+												for ($i = 1; $i <= 6; $i++) { 
 												?>
-									
-							<input type="text" name="nextNotifTo" value="<?php echo $nextNotifTo ?>" style="display: none;">
-							<input type="text" name="student_reg_no" value="<?php echo $student_reg_no ?>" style="display: none;">
+												<tr>
+													<td><?php echo $i ?></td>
 
-						</tbody>
-					</table>
+													<td><select class="form-control border-input" name="student<?php echo $i ?>" onchange="instaSearch(this.value, <?php echo $i ?>);">
+															<option value="">Select</option>
+															<?php
+															$query = "SELECT * FROM studentmaster";
+															$students = mysqli_query($connection, $query);
+
+															while( $thisStudent = mysqli_fetch_array($students)  )
+															{
+															?>
+																<option value="<?php echo $thisStudent['reg_no'] ?>"><?php 
+																	echo $thisStudent['reg_no']." - ".$thisStudent['name']; 
+																?>
+																</option>
+																<?php
+															}
+															?>
+														</select>	
+													</td>
+
+													<td>
+														<input id="<?php echo $i ?>1" name="studentRegno<?php echo $i ?>" class ="form-control border-input"/>
+													</td>
+
+													<td id=<?php echo $i ?>2>
+														
+													</td>
+
+													<td id=<?php echo $i ?>3>
+
+													</td>
+
+													<td id=<?php echo $i ?>4>
+														
+													</td>
+
+													<td id=<?php echo $i ?>5>
+														
+													</td>
+												</tr>
+												<?php
+											}
+											?>
+											<input type="text" name="nextNotifTo" value="<?php echo $nextNotifTo ?>" style="display: none;">
+										</tbody>
+									</table>
+								</div>
+							</div>
+							<div style="font-size:15px">
+								<div class="col-md-offset-8">(Signature of the Student)</div><br>
+								<div class="col-md-offset-1">Advised By: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Supervisor(s) </div><br>
+								<div class="col-md-offset-1">Forwarded By: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Convener DDPC&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Head of Department</div><br>
+								<div class="col-md-offset-1">Approved By: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chairman SDPC </div><br>
+
+
+
+							</div>
+
+							<div class="text-center">
+								<button type="submit" class="btn btn-info btn-fill btn-wd">Submit</button>
+							</div><br>
+							<h5 class="text-center" id="msg" style="color:red;"></h5>
+						</form>
+					</div>
+				</div>
+				<div>
 				</div>
 			</div>
-			<div style="font-size:15px">
-				<div class="col-md-offset-1">Proposed By: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Thesis Supervisor(s) </div><br>
-				<div class="col-md-offset-1">Forwarded By: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Convener DDPC&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Head of Department</div><br>
-				<div class="col-md-offset-1">Approved By: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chairman SDPC </div><br>
 
+		</div>
 
-
-			</div>
-
-			<div class="text-center">
-				<button type="submit" class="btn btn-info btn-fill btn-wd">Submit</button>
-			</div><br>
-			<h5 class="text-center" id="msg" style="color:red;"></h5>
-		</form>
 	</div>
-</div>
-<div>
-</div>
-</div>
-
-</div>
-
-</div>
 
 
-<footer class="footer">
-</footer>
+	<footer class="footer">
+	</footer>
 </div>
 </div>
 <p></p>
@@ -310,6 +381,21 @@
 <!-- <script src="assets/js/datepicker.js"></script> -->
 
 <script type="text/javascript">
+
+	$("#from_datepicker").datepicker({
+		minDate: 0,
+		dateFormat: 'yy-mm-dd',
+		onSelect: function(date) {
+			$("#to_datepicker").datepicker('option', 'minDate', date);
+		}
+	});
+
+	$("#to_datepicker").datepicker({ dateFormat: 'yy-mm-dd' });
+	$('#to_datepicker').change(function () {
+		var diff = $('#from_datepicker').datepicker("getDate") - $('#to_datepicker').datepicker("getDate");
+		$('#diff').val((diff / (1000 * 60 * 60 * 24) * -1) + 1);
+	});
+
 	function removeNot() {
 
 		$('.notificationAlert').css({
@@ -326,6 +412,22 @@
 		xmldata.send(null);
 		if(xmldata.responseText != ""){
 			toPrint = xmldata.responseText;
+		}
+	}
+	function checkform(form) {
+		var credits1 = document.getElementById(11).value;
+		var credits2 = document.getElementById(21).value;
+		var credits3 = document.getElementById(31).value;
+		var credits4 = document.getElementById(41).value;
+		var credits5 = document.getElementById(51).value;
+		var total_credits = Number(credits1) + Number(credits2) + Number(credits3) + Number(credits4) + Number(credits5);
+		if (total_credits < 8 || total_credits > 20) {
+			// alert(total_credits);
+			document.getElementById("msg").innerHTML = "Sum of credits is out of bound. Please fill correctly.";
+			return false;
+		} else {
+			document.dp01.submit();
+
 		}
 	}
 
