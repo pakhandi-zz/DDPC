e3d85388511deceb8a79569b91034bd528ebb0ab
pakhandi <asimkprasad@gmail.com>
2017-04-19 23:15:32 +0530

    Make notification DB based | more interactive

--- set_cookie.php
+++ set_cookie.php
@@ -1,12 +1,22 @@
 <?php
 session_start();
-for($i = 1; $i <= $_GET['notid']; $i++)
+include("includes/connect.php");
+$notificationId = $_GET['notid'];
+$query = "UPDATE notifications SET `read`='1' WHERE id =$notificationId";
+// echo $query;
+$result = mysqli_query($connection, $query);
+// echo "done!";
+
+$thisRole = $_SESSION['role'];
+$thisUniqueId = $_SESSION['reg_no'];
+$query = "SELECT * FROM notifications WHERE (target_group='$thisRole' OR target_member='$thisUniqueId') AND `read`='0' ORDER BY issue_date DESC";
+$allNotifications = mysqli_query($connection, $query);
+
+$countNotifications = 0;
+while( $notification = mysqli_fetch_array($allNotifications) )
 {
-	$cookie_name = (string)$_SESSION['reg_no'].(string)$i;
-	echo $cookie_name;
-	$cookie_value = "read";
-	setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+    $countNotifications += 1;
 }
-	
-echo "done!";
+echo $countNotifications;
+
 ?>
